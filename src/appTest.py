93ea7ff70d47db881ba7142d038e704a1278db54
MattFame <matt@clarusway.com>
2020-11-03 15:38:01 +0300

    @classmethod

--- src/appTest.py
+++ src/appTest.py
@@ -4,18 +4,22 @@ import app
 
 class Test(unittest.TestCase):
     
+    @classmethod
     def test_insert(self):
         inserted_person = app.insert_mock_name("jason",5559990101)
         self.assertEqual(inserted_person, 'Person Jason added to Phonebook successfully')
     
-    # def test_insert_person(self):
-    #     inserted_person = app.insert_person("jason",5559990101)
-    #     self.assertEqual(inserted_person, 'Person Jason added to Phonebook successfully')
+    @classmethod
+    def test_insert_person(self):
+        inserted_person = app.insert_person("jason",5559990101)
+        self.assertEqual(inserted_person, 'Person Jason added to Phonebook successfully')
     
+    @classmethod
     def test_update_person(self):
         updated_person = app.update_person("jason",5559990102)
         self.assertEqual(updated_person, 'Phone record of Jason is updated successfully')
     
+    @classmethod
     def test_delete_person(self):
         deleted_person = app.delete_person("jason")
         self.assertEqual(deleted_person, 'Phone record of Jason is deleted from the phonebook successfully')
