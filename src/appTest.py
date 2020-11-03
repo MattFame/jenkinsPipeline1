06ee479d45e7e470baea21c13b29e7001110b8ed
MattFame <matt@clarusway.com>
2020-11-03 15:14:31 +0300

    test added

--- src/appTest.py
+++ src/appTest.py
@@ -5,12 +5,16 @@ import app
 class Test(unittest.TestCase):
     
     def test_insert(self):
-        inserted_person = app.insert_mock_name("jason",5)
+        inserted_person = app.insert_mock_name("jason",5559990101)
         self.assertEqual(inserted_person, 'Person Jason added to Phonebook successfully')
     
     def test_insert_person(self):
-        inserted_person = app.insert_person("jason",5)
+        inserted_person = app.insert_person("jason",5559990101)
         self.assertEqual(inserted_person, 'Person Jason added to Phonebook successfully')
+    
+    def test_update_person(self):
+        updated_person = app.update_person("jason",5559990102)
+        self.assertEqual(updated_person, 'Phone record of Jason is updated successfully')
 
 if __name__ == '__main__':
     unittest.main()
